e3d4fdafa81e1706f521068f650bdfd9c891742a
dhanalakshmi <dhanalakshmi11012001@gmail.com>
2023-05-20 19:21:13 +0530

    Updates

---
+++ precache-manifest.68098c4c65023a44182ed2eb7041c2e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6fe4db9e9105e7910b",
+    "url": "/static/css/main.a36aef93.chunk.css"
+  },
+  {
+    "revision": "0e6fe4db9e9105e7910b",
+    "url": "/static/js/main.0e6fe4db.chunk.js"
+  },
+  {
+    "revision": "ad5daa36b2e98b7cd19f",
+    "url": "/static/js/1.ad5daa36.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "392ea8458e7e89a79f439e775bc16022",
+    "url": "/static/media/group16x9.392ea845.jpg"
+  },
+  {
+    "revision": "c364f73294afd251f50846ef61636904",
+    "url": "/static/media/pair-programming10x8.c364f732.jpg"
+  },
+  {
+    "revision": "1f473d6c1abdf3e617631e5a6f390e80",
+    "url": "/static/media/logo.1f473d6c.png"
+  },
+  {
+    "revision": "1b983935a49282de35df7bd49aaf8fde",
+    "url": "/index.html"
+  }
+];
